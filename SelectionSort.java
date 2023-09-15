4dd4c52600ad1a2233206bdda367ad00e2dc97e8
Baljeet Kumar <112314188+BaljeetKumarSingh@users.noreply.github.com>
2023-09-15 17:36:36 +0530

    Formatted SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,51 +1,72 @@
 import java.util.Scanner;
-public class SelectionSort{
-    public static int[] selectionSort(int[] arr){
+
+public class SelectionSort {
+    
+    // Creating a method for Selection Sort 
+    public static int[] selectionSort(int[] arr) {
         
-        for (int i=0;i<arr.length;i++){                    
+        for ( int i = 0 ; i < arr.length ; i++) {                    
 
             int minIndex = i;
-            for(int j=i+1;j<arr.length;j++){
+            
+            // Finding the index of minimum number in unsorted array
+            for( int j = i + 1 ; j < arr.length; j++) {
 
-                if (arr[j] < arr[minIndex]){                      
+                if ( arr[j] < arr[minIndex] ){                      
                     
-                    minIndex = j;                     //here we are finding the index of minimun element
+                    minIndex = j;
+                    
                 }
-            }
-            if (minIndex != i){
                 
-                // Here we are swaping the minimum element with the first element of unsorted array
+            }
+            
+            if (minIndex != i) {
+
+                // Swaping the minimum element with first element of unsorted array
                 int temp = arr[minIndex];
                 arr[minIndex] = arr[i];
                 arr[i] = temp;
+                
             }
+            
         }
+        
         return arr;
     }
-    public static void printArray(int[] arr){
-        for(int i=0;i<arr.length;i++){
+
+    // Creating a method to print the element of array
+    public static void printArray(int[] arr) {
+        
+        for( int i = 0 ; i < arr.length ; i++ ) {
+            
             System.out.print(arr[i]+" ");
+            
         }
+        
     }
-    public static void main(String[] args){
+    
+    public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         
         System.out.print("Enter the length of your array: ");
         int len = sc.nextInt();
 
+        // Creating an array
         int [] arr = new int[len];
 
         System.out.println("Enter the "+len+" elements: ");
 
-        for (int i=0;i<arr.length;i++){
+        // Taking element of array as an input from user
+        for ( int i = 0 ; i < arr.length ; i++ ) {
+            
             arr[i] = sc.nextInt();
+            
         }
 
-        //Calling the selectionSort function
+        // Calling the selectionSort function
         printArray(selectionSort(arr));
 
-
-
-
     }
+    
 }
